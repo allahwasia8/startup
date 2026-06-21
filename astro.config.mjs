02c3f4ee7bfc3c364f3c6ca56eb25abf1f529a1e
unknown <princealiomer@gmail.com>
2026-06-21 14:33:16 +0500

    Fix trailing slash in sitemap URLs to match canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ukstartupflow.com',
-  integrations: [sitemap()]
+  integrations: [sitemap({
+    serialize(item) {
+      if (item.url !== 'https://ukstartupflow.com/') {
+        item.url = item.url.replace(/\/$/, '');
+      }
+      return item;
+    }
+  })]
 });
